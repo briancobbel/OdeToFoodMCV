ba7540a69ec6b3d0f0de48400b5fffbb610ab424
bcobbel <bcobbel@gmail.com>
2021-06-13 18:18:28 -0500

    through module 7

---
+++ OdeToFood.Data/Models/CuisineType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data.Models
+{
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        Indian,
+        French
+    }
+}
---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,48 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+                {
+                    new Restaurant { Id = 1, Name = "Brian's Pizza", Cuisine = CuisineType.Italian},
+                    new Restaurant { Id = 2, Name = "Tersiguels", Cuisine = CuisineType.French},
+                    new Restaurant { Id = 3, Name = "India Cafe", Cuisine = CuisineType.Indian}
+                };
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            restaurants.Add(restaurant);
+            restaurant.Id = restaurants.Max(r => r.Id) + 1;
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var exisiting = Get(restaurant.Id);
+            if(exisiting != null)
+            {
+                exisiting.Name = restaurant.Name;
+                exisiting.Cuisine = restaurant.Cuisine;
+            }
+        }
+
+        public Restaurant Get(int id)
+        {
+            return restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
